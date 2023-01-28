757de6af7c282652cfc7d5bad7e6fc9a01ca3a75
Luke Sheppard <sheppiegit@gmail.com>
2023-01-28 15:41:11 -0500

    Added login route for a user. It checks the username and password and verifies if the user exists. Does not have error handling right now.

--- routes/auth.js
+++ routes/auth.js
@@ -20,5 +20,19 @@ router.post("/register", async (req, res) => {
 });
 
 //Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong credentials");
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("wrong password");
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
