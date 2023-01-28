6ad8a8735b6cb0fa08f9637a68b6bbe3caab7c42
Luke Sheppard <sheppiegit@gmail.com>
2023-01-27 20:31:25 -0500

    Connected MongDB to express server.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const server = express();
 const port = 3000;
 
-server.listen(port, () =>{
-    console.log("Server is read to rip")
-})
+mongoose.connect(process.env.MONGODB_CONNECTION, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("MongoDB connected");
+});
+
+server.use(express.json());
+
+server.listen(port, () => {
+  console.log("Server is read to rip");
+});
