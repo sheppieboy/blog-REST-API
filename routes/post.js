78114d512962b79680212d02fa9fe8a6a9497b11
Luke Sheppard <sheppiegit@gmail.com>
2023-01-28 18:04:59 -0500

    Added a post route to create a post.

---
+++ routes/post.js
@@ -0,0 +1,26 @@
+const Post = require("../models/Post");
+const router = require("express").Router();
+
+//create post
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const post = newPost.save();
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get a post
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const post = await Post.findById(req.params.id);
+//     res.status(200).json(post);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+//delete a post
+//get feed?
