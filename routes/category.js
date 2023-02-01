633158f585cb4a28b9d583ffc6b057e4f1fa42ee
Luke Sheppard <sheppiegit@gmail.com>
2023-01-31 20:21:26 -0500

    Added a route in category.js to create a Category.

---
+++ routes/category.js
@@ -0,0 +1,23 @@
+const router = "express".Router();
+const Category = require("../models/Category");
+
+router.post("/", async (req, res) => {
+  const newCategory = new Category(req.body);
+  try {
+    const savedCategory = await newCategory.save();
+    res.status(200).json("Saved Category");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// router.get("/", async (req, res) => {
+//   try {
+//     const category = await Category.find();
+//     res.status(200).json(category);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+module.exports = router;
